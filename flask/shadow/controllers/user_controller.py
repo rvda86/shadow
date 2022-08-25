8aa2ba71fa12a51c9feffbe76a4ba0438903067a
Robin van den Aardweg <rvda@protonmail.com>
2022-08-25 21:12:05 +0200

    bugfix

--- flask/shadow/controllers/user_controller.py
+++ flask/shadow/controllers/user_controller.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from shadow.models.user.users import User
 from shadow.validation import preprocess_incoming_data
 from shadow.models.entry.entries import get_all_categories_by_user, to_dict
 from shadow.utils import send_email_verification_mail, send_password_reset_mail
