867e58a66cba782cc04a232b1d042be58ef7a10d
Robin van den Aardweg <rvda@protonmail.com>
2022-09-03 20:54:08 +0200

    ui update

--- vue/src/components/pages/PasswordReset.vue
+++ vue/src/components/pages/PasswordReset.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card">
+    <div class="card card-grey">
         <div>
             <h3>Enter your new password:</h3>
             <form class="flex" @submit.prevent="submitHandler">
