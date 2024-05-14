372b0955fdf00b61fdd6962e388d2eb99028e8c7
Robin van den Aardweg <rvda@protonmail.com>
2024-05-14 21:55:06 +0200

    moved password validation to seperate file

--- flask/app/validation.py
+++ flask/app/validation.py
@@ -72,41 +72,6 @@ def validate_email(email):
     if not re.search("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
         raise InvalidDataError("invalid email address")
 
-
-common_passwords = ["password", "12345678", "123456789", "baseball", "football", "qwertyuiop", "1234567890",
-                    "superman", "1qaz2wsx", "trustno1", "jennifer", "sunshine", "iloveyou", "starwars",
-                    "computer", "michelle", "11111111", "princess", "987654321", "qwerty123", "aa12345678",
-                    "password1", "password123"]
-max_password_length = 128
-min_password_length = 8
-
-
-def validate_password(password: str) -> bool:
-    if not isinstance(password, str):
-        raise InvalidDataError("invalid password")
-    if password_is_too_common(password):
-        raise InvalidDataError(ExceptionMessages.PASSWORD_TOO_COMMON)
-    if password_is_entirely_numeric(password):
-        raise InvalidDataError(ExceptionMessages.PASSWORD_NUMERIC)
-    if re.search("^(.{0,7}|[^0-9]*|[^A-Za-z]*)$", password):
-        raise InvalidDataError("invalid password")
-    password_valid = True
-    return password_valid
-
-
-def password_is_entirely_numeric(password: str) -> bool:
-    if len(password) == 0:
-        return False
-    for char in password:
-        if char not in "1234567890":
-            return False
-    return True
-
-
-def password_is_too_common(password: str) -> bool:
-    return password in common_passwords
-
-
 def validate_username(username):
     if not isinstance(username, str):
         raise InvalidDataError("invalid username")
---
+++ flask/app/validators/password.py
@@ -0,0 +1,37 @@
+import re
+
+from app.constants.ExceptionMessages import ExceptionMessages
+from app.error_handling import InvalidDataError
+
+common_passwords = ["password", "12345678", "123456789", "baseball", "football", "qwertyuiop", "1234567890",
+                    "superman", "1qaz2wsx", "trustno1", "jennifer", "sunshine", "iloveyou", "starwars",
+                    "computer", "michelle", "11111111", "princess", "987654321", "qwerty123", "aa12345678",
+                    "password1", "password123"]
+max_password_length = 128
+min_password_length = 8
+
+
+def validate_password(password: str) -> bool:
+    if not isinstance(password, str):
+        raise InvalidDataError("invalid password")
+    if password_is_too_common(password):
+        raise InvalidDataError(ExceptionMessages.PASSWORD_TOO_COMMON)
+    if password_is_entirely_numeric(password):
+        raise InvalidDataError(ExceptionMessages.PASSWORD_NUMERIC)
+    if re.search("^(.{0,7}|[^0-9]*|[^A-Za-z]*)$", password):
+        raise InvalidDataError("invalid password")
+    password_valid = True
+    return password_valid
+
+
+def password_is_entirely_numeric(password: str) -> bool:
+    if len(password) == 0:
+        return False
+    for char in password:
+        if char not in "1234567890":
+            return False
+    return True
+
+
+def password_is_too_common(password: str) -> bool:
+    return password in common_passwords
