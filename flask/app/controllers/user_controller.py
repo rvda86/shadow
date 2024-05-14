c0bcd0fe2c43cbcef573b51001f35c09616498ce
Robin van den Aardweg <rvda@protonmail.com>
2024-05-14 21:30:23 +0200

    usercontroller only sends mails if MAIL_ENABLED

--- flask/app/controllers/user_controller.py
+++ flask/app/controllers/user_controller.py
@@ -1,8 +1,10 @@
 from flask import jsonify
-from app.models.user.User import User
-from app.validation import preprocess_incoming_data
+
+from app.config import Config
 from app.models.entry.entries import get_all_categories_by_user, to_dict
+from app.models.user.User import User
 from app.utils import send_email_verification_mail, send_password_reset_mail
+from app.validation import preprocess_incoming_data
 
 
 class UserController:
@@ -15,14 +17,31 @@ class UserController:
         user = User()
         user = user.create(data)
         response = {"data": user.as_dict_private_profile(), "msg": "account created, you can now log in"}
-        send_email_verification_mail(user)
+        if Config.MAIL_ENABLED:
+            send_email_verification_mail(user)
         return jsonify(response)
 
     @staticmethod
-    def get_user(user_id):
+    def delete_user(user_id, data):
+        if isinstance(data, dict):
+            data["type"] = "user"
+        data = preprocess_incoming_data(data, "DELETE")
         user = User()
         user.load_by_id(user_id)
-        return jsonify({"username": user.get_username(), "email": user.get_email(), "email_verified": user.get_email_verified()})
+        user.authenticate(data["password"])
+        response = user.delete(user_id)
+        return jsonify(response)
+
+    @staticmethod
+    def email_verification(email):
+        user = User()
+        user.load_by_email(email)
+        user.set_email_verified(True)
+        if Config.MAIL_ENABLED:
+            response = user.update_email_verification_status()
+        else:
+            response = "email verification currently disabled."
+        return jsonify(response)
 
     @staticmethod
     def email_verification_send_link(user_id):
@@ -32,19 +51,41 @@ class UserController:
         return jsonify({"msg": response})
 
     @staticmethod
+    def get_all_data_by_user(user_id: str):
+        user = User()
+        user.load_by_id(user_id)
+        user_data = {"username": user.get_username(), "email": user.get_email(),
+                     "email_verified": user.get_email_verified()}
+        user_categories = [to_dict(category) for category in get_all_categories_by_user(user_id)]
+        return jsonify({"user_data": user_data, "user_categories": user_categories})
+
+    @staticmethod
+    def get_user(user_id):
+        user = User()
+        user.load_by_id(user_id)
+        return jsonify({"username": user.get_username(), "email": user.get_email(),
+                        "email_verified": user.get_email_verified()})
+
+    @staticmethod
+    def get_token(data):
+        if isinstance(data, dict):
+            data["type"] = "token"
+        data = preprocess_incoming_data(data, "POST")
+        user = User()
+        user.load_by_username(data["username"])
+        user.authenticate(data["password"])
+        response = user.get_token()
+        return jsonify(response)
+
+    @staticmethod
     def password_reset_send_link(data):
         user = User()
         user.load_by_email(data["email"])
-        response = send_password_reset_mail(user)
+        if Config.MAIL_ENABLED:
+            response = send_password_reset_mail(user)
+        else:
+            response = "password reset currently disabled."
         return jsonify({"msg": response})
-
-    @staticmethod
-    def email_verification(email):
-        user = User()
-        user.load_by_email(email)
-        user.set_email_verified(True)
-        response = user.update_email_verification_status()
-        return jsonify(response)
 
     @staticmethod 
     def reset_password(data):
@@ -68,35 +109,3 @@ class UserController:
             user.update_email_verification_status()
             send_email_verification_mail(user)
         return jsonify(response)
-
-    @staticmethod
-    def delete_user(user_id, data):
-        if isinstance(data, dict): 
-            data["type"] = "user"
-        data = preprocess_incoming_data(data, "DELETE")
-        user = User()
-        user.load_by_id(user_id)        
-        user.authenticate(data["password"])
-        response = user.delete(user_id)
-        return jsonify(response)
-
-    @staticmethod 
-    def get_token(data):
-        if isinstance(data, dict): 
-            data["type"] = "token"
-        data = preprocess_incoming_data(data, "POST")
-        user = User()
-        user.load_by_username(data["username"])
-        user.authenticate(data["password"])
-        response = user.get_token()
-        return jsonify(response)
-
-    @staticmethod
-    def get_all_data_by_user(user_id: str):
-        user = User()
-        user.load_by_id(user_id)
-        user_data = {"username": user.get_username(), "email": user.get_email(), "email_verified": user.get_email_verified()}
-        user_categories = [to_dict(category) for category in get_all_categories_by_user(user_id)]
-        return jsonify({"user_data": user_data, "user_categories": user_categories})
-
-  
