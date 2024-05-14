dd796000c92e77f4dbbca923ea6491461bbe7a86
Robin van den Aardweg <rvda@protonmail.com>
2024-05-14 21:25:21 +0200

    add MAIL_ENABLED env variable

--- flask/app/config.py
+++ flask/app/config.py
@@ -1,23 +1,24 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_TESTING = os.getenv("DB_TESTING")
-    DB_PRODUCTION = os.getenv("DB_PRODUCTION")
-    TEST_ENVIRONMENT = "True" in os.getenv("TEST_ENVIRONMENT")
     API_LINK = os.getenv("API_LINK")
-    PASSWORD_RESET_LINK = os.getenv("PASSWORD_RESET_LINK")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PRODUCTION = os.getenv("DB_PRODUCTION")
+    DB_TESTING = os.getenv("DB_TESTING")
+    DB_USER = os.getenv("DB_USER")
     EMAIL_VERIFICATION_LINK = os.getenv("EMAIL_VERIFICATION_LINK")
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = os.getenv("MAIL_PORT")
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
-    MAIL_USE_TLS = "True" in os.getenv("MAIL_USE_TLS")
-    MAIL_USE_SSL = "True" in os.getenv("MAIL_USE_SSL")
+    MAIL_ENABLED = bool(int(os.getenv("MAIL_ENABLED")))
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_USE_SSL = bool(int(os.getenv("MAIL_USE_SSL")))
+    MAIL_USE_TLS = bool(int(os.getenv("MAIL_USE_TLS")))
+    PASSWORD_RESET_LINK = os.getenv("PASSWORD_RESET_LINK")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    TEST_ENVIRONMENT = bool(int(os.getenv("TEST_ENVIRONMENT")))
