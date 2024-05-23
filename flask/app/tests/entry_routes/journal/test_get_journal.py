348e40eb4c7724eefecb7abaefa6007cfea58360
Robin van den Aardweg <rvda@protonmail.com>
2024-05-23 15:47:19 +0200

    new tests for retrieving journal

---
+++ flask/app/tests/entry_routes/journal/test_get_journal.py
@@ -0,0 +1,47 @@
+import unittest
+
+from app.db_mysql import db_pool
+from app.tests.entry_routes.EntryRequester import EntryRequester
+from app.tests.helpers import create_user
+from app.tests.user_routes.UserRequester import UserRequester
+from app.utils.utils import uuid_generator
+
+
+# /api/entries GET
+class TestGetJournal(unittest.TestCase):
+
+    db = db_pool.acquire()
+    user_requester = UserRequester()
+    requester = EntryRequester()
+
+    def setUp(self):
+        if self.db.name != "shadow_testing":
+            raise Exception
+        self.token_1, _ = create_user(self.user_requester, "user1@example.com", "passwSf2@ord", "user1")
+        self.token_2, _ = create_user(self.user_requester, "user2@example.com", "passwSf2@ord", "user2")
+        self.category, _ = self.requester.create_entry({"type": "category", "name": "category1"}, self.token_1)
+        self.topic, _ = self.requester.create_entry({"type": "topic", "topic_type": "journal", "name": "topic_1",
+                                                     "category_id": self.category["entry"]["id"]}, self.token_1)
+        self.journal, _ = self.requester.create_entry({"type": "journal", "title": "My First Post",
+                                                       "content": "This is my first post",
+                                                       "topic_id": self.topic["entry"]["id"]}, self.token_1)
+
+    def tearDown(self):
+        self.db.reset_database()
+
+    def test_success(self):
+        data, status_code = self.requester.get_entry("journal", self.journal["entry"]["id"], self.token_1)
+        self.assertEqual(2003, status_code)
+
+    def test_combination_journal_author_unknown(self):
+        data, status_code = self.requester.get_entry("journal", self.journal["entry"]["id"], self.token_2)
+        self.assertEqual(4304, status_code)
+
+    def test_journal_does_not_exist(self):
+        journal_id = uuid_generator()
+        data, status_code = self.requester.get_entry("journal", journal_id, self.token_1)
+        self.assertEqual(4304, status_code)
+
+
+if __name__ == "__main__":
+    unittest.main()
