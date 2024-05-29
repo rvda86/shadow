e05eef2cc48954d8a40851cbf075d6da2119a4b0
Robin van den Aardweg <rvda@protonmail.com>
2024-05-29 14:02:18 +0200

    user_logger uses RotatingFileHandler

--- flask/app/logging.py
+++ flask/app/logging.py
@@ -1,13 +1,15 @@
 import logging
+from logging.handlers import RotatingFileHandler
 import time
 
 
-def get_user_logger():
+def get_user_logger() -> logging.Logger:
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
     formatter.converter = time.gmtime
-    file_handler = logging.FileHandler('./app/logs/user.log')
+
+    file_handler = RotatingFileHandler("./app/logs/user.log", maxBytes=2000000, backupCount=10)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     return logger
