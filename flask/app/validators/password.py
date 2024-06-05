e297aa2219f12855f1ee5eb1520d017522625aff
Robin van den Aardweg <rvda@protonmail.com>
2024-06-05 15:02:39 +0200

    validators use constants

--- flask/app/validators/password.py
+++ flask/app/validators/password.py
@@ -1,20 +1,14 @@
 from app.constants.ExceptionMessages import ExceptionMessages
+from app.constants.ValidationConstants import ValidationConstants
 from app.validators.string import string_is_too_short, string_is_too_long
-
-common_passwords = ["password", "12345678", "123456789", "baseball", "football", "qwertyuiop", "1234567890",
-                    "superman", "1qaz2wsx", "trustno1", "jennifer", "sunshine", "iloveyou", "starwars",
-                    "computer", "michelle", "11111111", "princess", "987654321", "qwerty123", "aa12345678",
-                    "password1", "password123"]
-max_password_length = 128
-min_password_length = 8
 
 
 def validate_password(password: str) -> str:
     if not isinstance(password, str):
         raise ValueError("not a string")
-    if string_is_too_short(min_password_length, password):
+    if string_is_too_short(ValidationConstants.MIN_PASSWORD_LENGTH, password):
         raise ValueError(ExceptionMessages.PASSWORD_TOO_SHORT)
-    if string_is_too_long(max_password_length, password):
+    if string_is_too_long(ValidationConstants.MAX_PASSWORD_LENGTH, password):
         raise ValueError(ExceptionMessages.PASSWORD_TOO_LONG)
     if password_is_too_common(password):
         raise ValueError(ExceptionMessages.PASSWORD_TOO_COMMON)
@@ -33,4 +27,4 @@ def password_is_entirely_numeric(password: str) -> bool:
 
 
 def password_is_too_common(password: str) -> bool:
-    return password in common_passwords
+    return password in ValidationConstants.COMMON_PASSWORDS
--- flask/app/validators/username.py
+++ flask/app/validators/username.py
@@ -1,8 +1,6 @@
 from app.constants.ExceptionMessages import ExceptionMessages
+from app.constants.ValidationConstants import ValidationConstants
 from app.validators.string import string_is_too_short, string_is_too_long, string_is_not_alphanumeric
-
-max_username_length = 30
-min_username_length = 2
 
 
 def validate_username(username: str) -> str:
@@ -10,9 +8,8 @@ def validate_username(username: str) -> str:
         raise ValueError("not a string")
     if string_is_not_alphanumeric(username):
         raise ValueError(ExceptionMessages.USERNAME_ILLEGAL_CHARACTERS)
-    if string_is_too_short(min_username_length, username):
+    if string_is_too_short(ValidationConstants.MIN_USERNAME_LENGTH, username):
         raise ValueError(ExceptionMessages.USERNAME_TOO_SHORT)
-    if string_is_too_long(max_username_length, username):
+    if string_is_too_long(ValidationConstants.MAX_USERNAME_LENGTH, username):
         raise ValueError(ExceptionMessages.USERNAME_TOO_LONG)
     return username
-
