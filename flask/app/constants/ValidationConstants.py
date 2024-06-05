80775ce6cec528a4696f9472aef7bf71a765e73b
Robin van den Aardweg <rvda@protonmail.com>
2024-06-05 15:01:37 +0200

    new constants for input validation

---
+++ flask/app/constants/ValidationConstants.py
@@ -0,0 +1,13 @@
+class ValidationConstants:
+    COMMON_PASSWORDS = ["password", "12345678", "123456789", "baseball", "football", "qwertyuiop", "1234567890",
+                        "superman", "1qaz2wsx", "trustno1", "jennifer", "sunshine", "iloveyou", "starwars",
+                        "computer", "michelle", "11111111", "princess", "987654321", "qwerty123", "aa12345678",
+                        "password1", "password123"]
+    MAX_JOURNAL_LENGTH = 65535
+    MIN_JOURNAL_LENGTH = 1
+    MAX_PASSWORD_LENGTH = 128
+    MIN_PASSWORD_LENGTH = 8
+    MAX_TITLE_LENGTH = 30
+    MIN_TITLE_LENGTH = 1
+    MAX_USERNAME_LENGTH = 30
+    MIN_USERNAME_LENGTH = 2
