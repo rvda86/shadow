a2ac65b2746f6f7b87183028e90e8d85f25b51ef
Robin van den Aardweg <rvda@protonmail.com>
2024-06-05 15:00:18 +0200

    name validator changed to string validator

--- flask/app/validators/string.py
+++ flask/app/validators/string.py
@@ -1,3 +1,19 @@
+from app.constants.ExceptionMessages import ExceptionMessages
+
+
+def validate_string(string, min_length, max_length):
+    if not isinstance(string, str):
+        raise ValueError("not a string")
+    if string_is_not_alphanumeric(string):
+        raise ValueError(ExceptionMessages.NAME_ILLEGAL_CHARACTERS)
+    if string_is_too_short(min_length, string):
+        raise ValueError(ExceptionMessages.NAME_TOO_SHORT)
+    if string_is_too_long(max_length, string):
+        raise ValueError(ExceptionMessages.NAME_TOO_LONG)
+
+    return string
+
+
 def string_is_not_alphanumeric(string: str) -> bool:
     for char in string:
         if not char.isalnum():
